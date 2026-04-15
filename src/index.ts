7908ec0134fd7ecb3f52759380194fc768904fcf
ernestoly78 <ernestoly@gmail.com>
2026-04-15 00:10:12 -0400

    Update index.ts

--- src/index.ts
+++ src/index.ts
@@ -1,9 +1,11 @@
 import express from "express";
 import coachRoutes from "./routes/coach.routes";
+import toolsRoutes from "./routes/tools.routes";
 
 const app = express();
-app.use(express.json());
 
+app.use(express.json());
+app.use("/tools", toolsRoutes);
 app.use("/coach", coachRoutes);
 
 app.get("/", (_, res) => {
